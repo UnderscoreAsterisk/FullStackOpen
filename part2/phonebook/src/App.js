21a9395f6001d9ef707c2643f08a504f0f45095a
unknown <apoorvgoel_bt2k16@dtu.ac.in>
2020-08-11 15:04:04 +0530

    phonebook step11 and step12

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,6 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import contactsSrv from './services/contacts'
 
+const SuccessNotif = ({message}) => {
+  if (message === null) return null
+
+  const msgStyle = {
+      color: "green",
+      background: "lightgrey",
+      fontSize: 20,
+      borderStyle: "solid",
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10
+  }
+
+  return (
+    <div style={msgStyle}>
+      {message}
+    </div>
+  )
+}
+
+const ErrorNotif = ({message}) => {
+  if (message === null) return null
+
+  const msgStyle = {
+      color: "red",
+      background: "lightgrey",
+      fontSize: 20,
+      borderStyle: "solid",
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10
+  }
+
+  return (
+    <div style={msgStyle}>
+      {message}
+    </div>
+  )
+}
+
 const People = ({persons, searchName, onDelete}) => {
   const filtered = persons.filter((person) => {
     return  searchName.length === 0 || 
@@ -51,7 +91,9 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ searchName, setSearchName] = useState('')
-  
+  const [ message, setMessage ] = useState(null)
+  const [ error, setError ] = useState(null)
+
   useEffect(() => {
     contactsSrv.getAll().then((response) => setPersons(response))
   }, [])
@@ -62,8 +104,14 @@ const App = () => {
 
   const deletePersonHandler = (p) => {
     if(window.confirm(`Delete ${p.name}?`)) {
-      contactsSrv.remove(p.id).then(() => console.log(`Removed ${p.name}`))
-      setPersons(persons.filter((e) => e.id !== p.id))
+      contactsSrv.remove(p.id).then(() => {
+        console.log(`Removed ${p.name}`)
+        setPersons(persons.filter((e) => e.id !== p.id))})
+        .catch(() => {
+          setError(`${p.name} no longer exists on the server`)
+          setTimeout(() => setError(null), 5000)
+        })
+    
     }
   }
 
@@ -75,17 +123,30 @@ const App = () => {
       if (window.confirm(`${newName} already exists, do you want update the number?`)) {
         const newObj = {name: newName, number: newNumber}
         const pid = persons[i].id
-        contactsSrv.update(pid, newObj).then((response) => setPersons(persons.filter((p) => p.id !== pid).concat(response)))
+        contactsSrv.update(pid, newObj).then((response) => {
+          setPersons(persons.filter((p) => p.id !== pid).concat(response))
+          setMessage(`Updated ${newName} to ${newNumber}`)
+          setTimeout(() => setMessage(null), 5000)
+        }).catch(() => {
+          setError(`${newName} no longer exists on the server`)
+          setTimeout(() => setError(null), 5000)
+        })
       }
 
     } else {
       const newPersonObj = {name: newName, number: newNumber}
-      contactsSrv.create(newPersonObj).then((response) => {setPersons(persons.concat(response))})
+      contactsSrv.create(newPersonObj).then((response) => {
+        setPersons(persons.concat(response))
+        setMessage(`Added ${newName}`)
+        setTimeout(() => setMessage(null), 5000)
+      })
     }
   }
 
   return (
     <div>
+      <SuccessNotif message={message}/>
+      <ErrorNotif message={error}/>
       <Search onChange={searchChangeHandler}/>
       <Form onSubmit={addPerson} onNameChange={nameChangeHandler} onNumberChange={numberChangeHandler}/>
       <People persons={persons} searchName={searchName} onDelete={deletePersonHandler}/>
